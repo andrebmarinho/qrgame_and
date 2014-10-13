b4fb32344e2095dff8740eb4d2486b3a686eb5d1
andrebmarinho <andrebezerramarinho@gmail.com>
2014-10-13 18:12:05 -0300

    Limpeza da branch

--- src-testes/br/unb/cic/qrgame/domain/TesteQRCode.java
+++
@@ -1,7 +0,0 @@
-package br.unb.cic.qrgame.domain;
-
-import junit.framework.TestCase;
-
-public class TesteQRCode extends TestCase {
-
-}
--- src/br/unb/cic/qrgame/core/ExceptionType.java
+++
@@ -1,5 +0,0 @@
-package br.unb.cic.qrgame.core;
-
-public enum ExceptionType {
-	SERVER, LOGIN, NONE
-}
